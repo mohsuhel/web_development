fad1bf874b78162709dabe07fdb44b95b79cde79
suhmd85 <suhmd8530@gmil.com>
2023-05-21 17:13:40 +0530

    comaprison,logical,Datatypes

---
+++ operators.js
@@ -0,0 +1,15 @@
+// write the which is the biggest among those nmbr using operators, comparsion operations(>,<,>=,<=,=!),logical(&&,||)
+
+// let is a keyword used for avoiding hoisting and re declartaion
+
+let x = 10;
+let y = 20;
+
+if(x>y && x==y) //comparison opertor nd logicl operator  is used
+{
+    console.log("x is greater than y")
+}
+else
+{
+    console.log("y is greater than x")
+}
